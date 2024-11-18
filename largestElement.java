0d833673c2f90ac3955b48a8e31d8a6f46e1961a
abhishek <abhishekit31@gmail.com>
2024-11-18 20:49:11 +0530

    find largest element

--- largestElement.java
+++ largestElement.java
@@ -2,8 +2,13 @@ class largestElement {
     public static void main(String[] args) {
         int arr[] = { 2, 8, 5, 63, 7 };
 
-        for (int i = 0; i <= arr.length; i++) {
-            System.out.println(arr[i]);
+        int largest = arr[0];
+        for (int i = 0; i <= arr.length - 1; i++) {
+
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
         }
+        System.out.println("The largest element is : " + largest);
     }
 }
