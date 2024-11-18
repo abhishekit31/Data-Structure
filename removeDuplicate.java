12782caec78017900a2bc44a66f3b80cad1be485
abhishek <abhishekit31@gmail.com>
2024-11-19 00:10:17 +0530

    remove the duplicate from array and print its length

---
+++ removeDuplicate.java
@@ -0,0 +1,18 @@
+public class removeDuplicate {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4 };
+        int i = 0;
+
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                arr[i + 1] = arr[j];
+                i++;
+            }
+        }
+        int newlength = i + 1;
+        System.out.println("lenght of the array");
+        for (int j = 0; j < newlength; j++) {
+            System.out.print(arr[j] + " ");
+        }
+    }
+}
