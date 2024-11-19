b97ed236e9f29d580d47ac68379246486d01df2b
abhishek <abhishekit31@gmail.com>
2024-11-19 22:36:10 +0530

    find the target element

---
+++ targetedElement.java
@@ -0,0 +1,18 @@
+public class targetedElement {
+    public static void main(String[] args) {
+        int target = 4;
+        int[] arr = { 1, 4, 5, 8, 4, 2, 3 };
+        boolean indexFound = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Element prsent at index :" + i);
+                indexFound = true;
+                break;
+            }
+        }
+        if (!indexFound) {
+            System.out.println("Element not present in the array");
+        }
+    }
+}
