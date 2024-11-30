01ae532ff24ec59cffa3ddf00689f2f34e804b40
abhishek <abhishekit31@gmail.com>
2024-11-30 20:20:42 +0530

    sort the element by using selection sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+public class SelectionSort {
+    public static void PrintArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 9, 2, 5, 6, 3, 1 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        PrintArray(arr);
+    }
+}
+
+// Time complexity :- O(n^2) {
