71dd5d55628b9d68c9cc954f6a1e8b097cf28645
abhishek <abhishekit31@gmail.com>
2024-12-05 19:27:42 +0530

    reduce the line of code

--- Array/UniqueElement.java
+++ Array/UniqueElement.java
@@ -1,22 +1,18 @@
 package Array;
 
 public class UniqueElement {
+
+    public static int findUnique(int[] arr) {
+        int res = 0;
+        for (int ele : arr) {
+            res ^= ele;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 1, 2, 3, 4 };
+        System.out.println(findUnique(arr));
 
-        for (int i = 0; i < arr.length; i++) {
-            boolean isUnique = true;
-
-            for (int j = 0; j < arr.length; j++) {
-                if (i != j && arr[i] == arr[j]) {
-                    isUnique = false;
-                    break;
-                }
-            }
-
-            if (isUnique) {
-                System.out.println(arr[i]);
-            }
-        }
     }
 }
